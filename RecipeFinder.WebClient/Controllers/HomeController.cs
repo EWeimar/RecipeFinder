86c6b2f4abbbf7107780767e77183e991febf03d
Nikolaj Sommer Jensen <nikolaj.jensen@gmail.com>
2020-11-11 12:03:43 +0100

    added util classes

--- RecipeFinder.WebClient/Controllers/HomeController.cs
+++ RecipeFinder.WebClient/Controllers/HomeController.cs
@@ -10,9 +10,7 @@ namespace RecipeFinder.WebClient.Controllers
     {
         public ActionResult Index()
         {
-            SetFlash(Models.FlashMessageType.Success, "Dette er en Flash-Message. Denne er blevet sat i HomeController/index metoden. Disse FlashMessages bliver renderet lige før view'et. Men bliver sat i Viewed eller Controller metoden. Forsvinder når brugeren har set dem én gang.");
-            //SetFlash(Models.FlashMessageType.Warning, "Flash Message: Warning");
-            //SetFlash(Models.FlashMessageType.Danger, "Flash Message: Danger/Error");
+            SetFlash(Models.FlashMessageType.Success, "flash test.");
 
             return View();
         }
