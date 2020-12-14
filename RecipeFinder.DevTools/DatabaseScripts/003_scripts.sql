d43029a42ddcf6345994ad0b53a1bd9ec8dd1858
Nikolaj Sommer Jensen <nikolaj.jensen@gmail.com>
2020-12-14 15:44:21 +0100

    tests initialized

---
+++ RecipeFinder.DevTools/DatabaseScripts/003_scripts.sql
@@ -0,0 +1,2 @@
+ALTER TABLE Recipe
+ADD UNIQUE (Slug);
---
+++ RecipeFinder.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RecipeFinder.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RecipeFinder.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f12a4fd-7f00-4664-8b58-420af136eac7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
